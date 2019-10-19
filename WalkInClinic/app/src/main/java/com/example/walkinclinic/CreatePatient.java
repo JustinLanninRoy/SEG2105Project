39c0cfbaf7dbfe50fb9f74045868d2ae79b201b2
Maxim <mkarp032@uottawa.ca>
2019-10-19 17:59:33 -0400

    Made changes to the CreatePatient class, making it more organized and more efficient. Implemented a checkEmpty method, and a checkValidEmail method. Email Validation is functional for this class.

---
+++ WalkInClinic/app/src/main/java/com/example/walkinclinic/CreatePatient.java
@@ -0,0 +1,120 @@
+package com.example.walkinclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class CreatePatient extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_patient);
+    }
+
+    EditText firstName;
+    EditText lastName;
+    EditText streetAddress;
+    EditText postalCode;
+    EditText Age;
+    EditText Email;
+    EditText Phone;
+    EditText userName;
+    EditText userPassword;
+
+    String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    public void onCreatePatient(View view){
+
+        //Go get the values of all the fields that are variables
+        firstName = findViewById(R.id.FirstName);
+        lastName = findViewById(R.id.LastName);
+        streetAddress = findViewById(R.id.StreetAddress);
+        postalCode = findViewById(R.id.postalCode);
+        Age = findViewById(R.id.Age);
+        Email = findViewById(R.id.Email);
+        Phone = findViewById(R.id.phoneNumber);
+        userName = findViewById(R.id.username);
+        userPassword = findViewById(R.id.password);
+
+        //check for all fields being entered
+        if(checkAllFields()){
+            Toast.makeText(getApplicationContext(),"Please fill in every category",Toast.LENGTH_LONG).show();
+        }
+        //check for valid email address
+        else if(!checkEmailValid(Email)){
+            Toast.makeText(getApplicationContext(), "Please enter a valid email", Toast.LENGTH_LONG).show();
+        }
+        //open the postlogin class and display the correct message
+        else {
+            //creating the string
+            String postLogginString = ("Welcome " + firstName.getText().toString() + "! You are logged-in as a patient");
+
+            //opening the PostLoggin class and sending the message with it
+            Intent i = new Intent(this, PostLoggin.class);
+            i.putExtra("message", postLogginString);
+            startActivity(i);
+            Toast.makeText(getApplicationContext(),"Account Created",Toast.LENGTH_LONG).show();
+        }
+    }
+
+    boolean checkEmailValid(EditText text){
+        //getting email as a CharSequence because patterns matcher takes a CharSequence
+        CharSequence emailString = text.getText().toString().trim();
+        //checking that the email isn't null and that the email matches the PATTERN of an email
+        //this returns true if the email field has something written in it and it looks like an email
+        return(Patterns.EMAIL_ADDRESS.matcher(emailString).matches());
+    }
+
+    boolean checkEmpty(EditText text){
+        //get the inside of the text field being checked
+        CharSequence s = text.getText().toString();
+        //returns true if the text field is just empty
+        return TextUtils.isEmpty(s);
+    }
+
+    //method to check if any field is left blank. Returns true if anything is checkEmpty = true, return false if everything is filled.
+    boolean checkAllFields(){
+        if(checkEmpty(firstName)){
+            return true;
+        }
+        else if(checkEmpty(lastName)){
+            return true;
+        }
+        else if (checkEmpty(streetAddress)) {
+            return true;
+        }
+        else if (checkEmpty(postalCode)){
+            return true;
+        }
+        else if (checkEmpty(Age)){
+            return true;
+        }
+        else if (checkEmpty(Email)){
+            return true;
+        }
+        else if (checkEmpty(Phone)){
+            return true;
+        }
+        else if (checkEmpty(userName)){
+            return true;
+        }
+        else if (checkEmpty(userPassword)){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+}
---
+++ WalkInClinic/app/src/main/java/com/example/walkinclinic/MainActivity.java
@@ -0,0 +1,97 @@
+package com.example.walkinclinic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import java.lang.String;
+import android.os.Bundle;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onLoggin(View view){
+
+        //getting the username input
+        TextView userName = (TextView) findViewById(R.id.txtEditUsername);
+        String username = userName.getText().toString().trim();
+
+        //getting the password input
+        TextView passWord = (TextView) findViewById(R.id.txtEditPassword);
+        String password = passWord.getText().toString().trim();
+
+        if(username.equalsIgnoreCase("admin") && password.equalsIgnoreCase("5T5ptQ")){
+            //getting the value of the radio button selected
+            RadioGroup radioAccountTypeGroup = (RadioGroup) findViewById(R.id.AccountType);
+
+            // get selected radio button from radioGroup
+            int selectedId = radioAccountTypeGroup.getCheckedRadioButtonId();
+
+            // find the radiobutton by returned id
+            RadioButton selectedRadioButtonAccount = (RadioButton) findViewById(selectedId);
+            String type = selectedRadioButtonAccount.getText().toString().trim();
+
+            //creating the string
+            String postLogginString = ("Welcome " + username + "! You are logged-in as a " + type);
+
+            //opening the PostLoggin class and sending the message with it
+            Intent i = new Intent(this, PostLoggin.class);
+            i.putExtra("message", postLogginString);
+            startActivity(i);
+        } else {
+            //give error warning to user
+            Toast.makeText(getApplicationContext(),"Account login is currently under development. The only way to log in is by selecting admin, inputting username: admin, password: 5T5ptQ",Toast.LENGTH_LONG).show();
+
+            //forcefully input loggin for now
+            TextView setUsername = (TextView) findViewById(R.id.txtEditUsername);
+            setUsername.setText("admin");
+            TextView userPassword = (TextView) findViewById(R.id.txtEditPassword);
+            userPassword.setText("5T5ptQ");
+
+            //forcefully select admin for now
+            RadioButton Admin = (RadioButton) findViewById(R.id.Admin);
+            Admin.setChecked(true);
+        }
+    }
+
+    public void onCreateAccount(View view){
+
+        //getting the value of the radio button selected
+        RadioGroup radioAccountTypeGroup = (RadioGroup) findViewById(R.id.AccountType);
+
+        // get selected radio button from radioGroup
+        int selectedId = radioAccountTypeGroup.getCheckedRadioButtonId();
+
+        // find the radiobutton by returned id
+        RadioButton selectedRadioButtonAccount = (RadioButton) findViewById(selectedId);
+
+        String type = selectedRadioButtonAccount.getText().toString().trim();
+
+        //selecting the correct window to open
+        if(type.equalsIgnoreCase("patient")){
+            Intent i = new Intent(this, CreatePatient.class);
+            startActivity(i);
+            Toast.makeText(getApplicationContext(),"Logged-In",Toast.LENGTH_LONG).show();
+        }
+        if(type.equalsIgnoreCase("Admin")){
+            Toast.makeText(getApplicationContext(),"New Admin account cannot be made please select Patient before creating a new account",Toast.LENGTH_LONG).show();
+        }
+        if(type.equalsIgnoreCase( "Employee")){
+            Intent i = new Intent(this, createEmployee.class);
+            startActivity(i);
+            Toast.makeText(getApplicationContext(),"Logged-In",Toast.LENGTH_LONG).show();
+        }
+    }
+}
