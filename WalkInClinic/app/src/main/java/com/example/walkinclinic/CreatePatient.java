c7c3cc98cdda08f6820885182d2bd5948a3b9193
majishka <majawitter00@gmail.com>
2019-11-04 14:08:40 -0500

    attempt at firebase, not working

--- WalkInClinic/app/src/main/java/com/example/walkinclinic/CreatePatient.java
+++ WalkInClinic/app/src/main/java/com/example/walkinclinic/CreatePatient.java
@@ -3,22 +3,62 @@ package com.example.walkinclinic;
 import android.os.Bundle;
 
 import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import java.util.ArrayList;
 import java.util.List;
 
 public class CreatePatient extends CreatePerson {
     List<Patient> patients;
+    DatabaseReference databasePatients;
+    EditText a;
+    EditText zip;
+    EditText streetAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_patient);
+        databasePatients = FirebaseDatabase.getInstance().getReference("patients");
+        patients = new ArrayList<>();
     }
 
     public void onCreatePerson(View view){
         super.onCreatePerson(view);
+        a = findViewById(R.id.Age);
+        zip = findViewById(R.id.postalCode);
+        streetAddress = findViewById(R.id.StreetAddress);
+        addPatient();
+    }
+
+    @Override
+    protected void onStart() {
+
+        super.onStart();
+        databasePatients.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot postSnapshot: dataSnapshot.getChildren()){
+                    Patient patient = postSnapshot.getValue(Patient.class);
+                    patients.add(patient);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     @Override
@@ -30,6 +70,22 @@ public class CreatePatient extends CreatePerson {
         i.putExtra("message", postLogginString);
         startActivity(i);
         Toast.makeText(getApplicationContext(),"Account Created",Toast.LENGTH_LONG).show();
+    }
+
+    private void addPatient() {
+        String fName = firstName.getText().toString().trim();
+        String lName = lastName.getText().toString().trim();
+        String address = streetAddress.getText().toString().trim();
+        String postalCode = zip.getText().toString().trim();
+        String value = a.getText().toString().trim();
+        int age = Integer.parseInt(value);
+        String email = Email.getText().toString().trim();
+        String phone = Phone.getText().toString().trim();
+        String user = userName.getText().toString().trim();
+        String password = userPassword.getText().toString().trim();
+        String id = databasePatients.push().getKey();
+        Patient patient = new Patient(id, fName, lName, address, postalCode, age, email, phone, user, password);
+        databasePatients.child(id).setValue(patient);
     }
 
 }
--- WalkInClinic/app/src/main/java/com/example/walkinclinic/Patient.java
+++ WalkInClinic/app/src/main/java/com/example/walkinclinic/Patient.java
@@ -1,28 +1,31 @@
 package com.example.walkinclinic;
 
 public class Patient {
-    private String fName;
-    private String lName;
-    private String address;
-    private String postalCode;
-    private int age;
-    private String email;
-    private String phone;
-    private String user;
-    private String password;
+    public String _fName;
+    public String _lName;
+    public String _address;
+    public String _postalCode;
+    public int _age;
+    public String _email;
+    public String _phone;
+    public String _user;
+    public String _password;
+    public String _iD;
 
     public Patient(){
 
     }
-    public Patient(String first, String last, String add, String zip, int x, String mail, String phoneNum, String userName, String pass){
-        fName = first;
-        lName = last;
-        address = add;
-        postalCode = zip;
-        age = x;
-        email = mail;
-        phone = phoneNum;
-        user = userName;
-        password = pass;
+
+    public Patient(String id, String first, String last, String add, String zip, int x, String mail, String phoneNum, String userName, String pass){
+        _iD = id;
+        _fName = first;
+        _lName = last;
+        _address = add;
+        _postalCode = zip;
+        _age = x;
+        _email = mail;
+        _phone = phoneNum;
+        _user = userName;
+        _password = pass;
     }
 }
